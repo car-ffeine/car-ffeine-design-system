0bedecee774dd347e7a01ed2ef0ecb4d708e52c2
feb-dain <feb.dain@gmail.com>
2023-10-20 02:47:50 +0900

    fix: 스토리북 배포 문제 개선

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,26 @@
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.ts'),
-      name: 'index',
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: ['src/stories/**', 'react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-        },
-      },
-    },
-    commonjsOptions: {
-      esmExternals: ['react'],
-    },
-  },
+  // build: {
+  //   lib: {
+  //     entry: path.resolve(__dirname, 'src/lib/index.ts'),
+  //     name: 'index',
+  //     fileName: 'index',
+  //   },
+  //   rollupOptions: {
+  //     external: ['src/stories/**', 'react', 'react-dom'],
+  //     output: {
+  //       globals: {
+  //         react: 'React',
+  //       },
+  //     },
+  //   },
+  //   commonjsOptions: {
+  //     esmExternals: ['react'],
+  //   },
+  // },
   plugins: [react(), dts(), tsconfigPaths()],
 });
